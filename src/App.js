8604ed8905e6f3168a91730d619a591bcdaa285d
Adrian Viquez <aviquez96@gmail.com>
2021-01-22 20:50:40 -0800

    This new branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Helkhkjha
+          New Change
         </p>
         <a
           className="App-link"
